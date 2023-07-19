bc863f13d3577536021a08b7c403091858773b68
c-heath1 <120684707+c-heath1@users.noreply.github.com>
2023-07-19 14:18:01 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-aws-tf-state-bucket23"
-    key = "main"
+    key = "master"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
   }
